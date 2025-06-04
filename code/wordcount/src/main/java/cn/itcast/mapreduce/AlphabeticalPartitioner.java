23f4073f9540b5d020fa54c7099922bbceb7af1f
Mr_yqz <1607053429@qq.com>
2025-06-04 22:39:43 +0800

    add 3.3 mapreduce code

---
+++ code/wordcount/src/main/java/cn/itcast/mapreduce/AlphabeticalPartitioner.java
@@ -0,0 +1,26 @@
+package cn.itcast.mapreduce;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class AlphabeticalPartitioner extends Partitioner<Text, LongWritable> {
+
+    @Override
+    public int getPartition(Text key, LongWritable value, int numPartitions) {
+        // 获取单词的第一个字符
+        String word = key.toString();
+        if (word == null || word.isEmpty()) {
+            return 0; // 如果单词为空或null，返回第0个分区
+        }
+        char firstChar = Character.toUpperCase(word.charAt(0));
+
+        // 计算分区编号，A-Z 对应 0-25
+        if (firstChar >= 'A' && firstChar <= 'Z') {
+            return firstChar - 'A';
+        } else {
+            // 对于非A-Z的字符，可以将其分配到最后一个分区或者特殊处理
+            return 25; // 这里我们将它们分配到最后一个分区
+        }
+    }
+}
---
+++ code/wordcount/src/main/java/cn/itcast/mapreduce/WordCountReducer.java
@@ -0,0 +1,29 @@
+package cn.itcast.mapreduce;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class WordCountReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
+
+    // 用于存储最终输出的 value
+    private final LongWritable result = new LongWritable();
+
+    @Override
+    protected void reduce(Text key, Iterable<LongWritable> values, Context context)
+            throws IOException, InterruptedException {
+
+        long sum = 0;
+
+        // 遍历所有值，进行累加
+        for (LongWritable value : values) {
+            sum += value.get(); // 累加每个单词出现的次数
+        }
+
+        // 设置结果并写出
+        result.set(sum);
+        context.write(key, result); // 输出 <单词, 总次数>
+    }
+}
